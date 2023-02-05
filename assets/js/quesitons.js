542f250d4017e3c8c50fd20076770a0226ab44cc
Liam Clague <liamclague@Liams-MacBook-Pro.local>
2023-02-05 19:36:46 +0000

    questions added to questions.js file

---
+++ assets/js/quesitons.js
@@ -0,0 +1,91 @@
+let questions = [
+    {
+        number: 1,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: [
+            "Clearly Seen Screen",
+            "Can See Screen",
+            "Correct Style Sheet",
+            "Cascading Style Sheet",
+        ]
+    },
+    {
+        number: 2,
+        question: "Which variable type stores data that is not changeable?",
+        answer: "let",
+        options: [
+            "const",
+            "let",
+            "var",
+            "expelliamus",
+        ]
+    },
+    {
+        number: 3,
+        question: "In which do you put an array?",
+        answer: "[]",
+        options: [
+            "{}",
+            "[]",
+            "<>",
+            "\/",
+        ]
+    },
+    {
+        number: 4,
+        question: "Which method creates a random number?",
+        answer: "Math.random()",
+        options: [
+            "Math.rand()",
+            "Number.random()",
+            "Math.random()",
+            "Number.rand()",
+        ]
+    },
+    {
+        number: 5,
+        question: "Which javascript method returns the length of a string?",
+        answer: ".length()",
+        options: [
+            ".length()",
+            ".spread()",
+            ".len()",
+            ".spr()",
+        ]
+    },
+    {
+        number: 6,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Markup Language",
+            "Hyper Text Markup Line",
+            "Hyper Text Markdown Language",
+            "Hyper Text Markdown Line",
+        ]
+    },
+    {
+        number: 7,
+        question: "What does concat stand for?",
+        answer: "Concatenate",
+        options: [
+            "Concator",
+            "COncatenate",
+            "Converse Catology",
+            "Content Category",
+        ]
+    },
+    {
+        number: 8,
+        question: "What is the shorthand for writing a javascript function?",
+        answer: "() => {}",
+        options: [
+            "() = {}",
+            "[] => {}",
+            "() => {}",
+            "[] = {}",
+        ]
+    },
+
+]
